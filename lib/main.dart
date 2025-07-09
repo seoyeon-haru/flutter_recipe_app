a93fde5f61bd4ea763aee1c6b0d7e22ff231bd41
seoyeon-haru <sy5792@naver.com>
2025-07-09 18:55:20 +0900

    Apply font

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSansKR'
+      ),
       home: RecipePage(),
     );
   }
