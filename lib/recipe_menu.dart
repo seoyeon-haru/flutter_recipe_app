be9ee8cb38d40553c241cbd7bc2e182f62d124cc
seoyeon-haru <sy5792@naver.com>
2025-07-09 20:31:25 +0900

    Set decoration for Container

--- lib/recipe_menu.dart
+++ lib/recipe_menu.dart
@@ -3,6 +3,17 @@ import 'package:flutter/material.dart';
 class RecipeMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('RecipeMenu');
+    return Row(
+      children: [
+        Container(
+          width: 60,
+          height: 80,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black12),
+            borderRadius: BorderRadius.circular(30),
+          ),
+        )
+      ],
+    );
   }
 }
