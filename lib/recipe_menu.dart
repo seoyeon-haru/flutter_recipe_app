e32086e6e848f43bc50fae2a61cc9a6601882bd6
seoyeon-haru <sy5792@naver.com>
2025-07-09 20:50:09 +0900

    Adjust item spacing

--- lib/recipe_menu.dart
+++ lib/recipe_menu.dart
@@ -6,34 +6,39 @@ class RecipeMenu extends StatelessWidget {
     return Row(
       children: [
         menuItem(Icons.food_bank, 'All'),
+        SizedBox(width: 25),
         menuItem(Icons.emoji_food_beverage, 'Coffee'),
+        SizedBox(width: 25),
         menuItem(Icons.fastfood, 'Burger'),
+        SizedBox(width: 25),
         menuItem(Icons.local_pizza, 'Pizza'),
-
+        SizedBox(width: 25),
       ],
     );
   }
 
   Container menuItem(IconData icon, String text) {
     return Container(
-        width: 60,
-        height: 80,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.black12),
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(
-              icon,
-              size: 30,
-              color: Colors.redAccent,
-            ),
-            SizedBox(height: 5,),
-            Text(text),
-          ],
-        ),
-      );
+      width: 60,
+      height: 80,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black12),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            size: 30,
+            color: Colors.redAccent,
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(text),
+        ],
+      ),
+    );
   }
 }
