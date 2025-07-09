ecb5991064adcf0f55b87b4e38e7679f3e3fb455
seoyeon-haru <sy5792@naver.com>
2025-07-09 16:13:43 +0900

    Align text to the left

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class RecipePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           RecipeTitle(),
           RecipeMenu(),
